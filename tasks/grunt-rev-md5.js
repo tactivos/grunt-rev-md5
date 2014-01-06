3263d5ad1b3345be3a8f1d3570936a46c079c0fe
Roy Tsabari <a300600@gmail.com>
2014-01-06 19:33:59 +0200

    use options in task registration (a fix)

--- tasks/grunt-rev-md5.js
+++ tasks/grunt-rev-md5.js
@@ -11,14 +11,7 @@ module.exports = function (grunt) {
 	var path = require('path');
 	var crypto = require('crypto');
 
-	var supportedTypes = options.supportedTypes || {
-		html: 'html',
-		css: 'css',
-		soy: 'html',
-		ejs: 'html',
-		cshtml: 'html',
-		spark: 'html'
-	};
+	
 
 	var reghtml = new RegExp(/<(?:img|link|source|script).*\b(?:href|src)\b=['"]([^ ]+)['"].*\/?>/ig);
 	var regcss = new RegExp(/url\(([^)]+)\)/ig);
@@ -28,7 +21,15 @@ module.exports = function (grunt) {
 
 	grunt.registerMultiTask('revmd5', 'Appends a cache busting ?v={MD5} hash to the file reference', function () {
 		var self = this;
-
+		
+		var supportedTypes = options.supportedTypes || {
+			html: 'html',
+			css: 'css',
+			soy: 'html',
+			ejs: 'html',
+			cshtml: 'html',
+			spark: 'html'
+		};
 		var options = this.options({
 			encoding: 'utf8',
 			safe: false
